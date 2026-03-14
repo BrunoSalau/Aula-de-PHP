7c22de5b019a0c7742232fe29f5adaa103167afd
BrunoSalau <brunosalau@gmail.com>
2026-03-13 21:41:00 -0300

    Add files via upload

---
+++ Aula13.03.26/index.php
@@ -0,0 +1,10 @@
+<?php
+    echo "Olá mundo!!";
+    $idade = 20;
+    $nome = "Babajuur";
+    echo '<p>Ola '.$nome.' sua idade é '.$idade.'</p>';
+    echo "<p>Olá $nome sua idade é $idade</p>";
+?>
+<a href="formulario.html" target="_blank">
+    Fomulario
+</a>
